09e79f523b8a52b912d14690f16bf725e3a92184
Basile Bruneau <basilebruneau@gmail.com>
2019-01-02 23:23:48 +0100

    Set correct homepage for docs

---
+++ docs/precache-manifest.36651e83e60ef28d7a4428c6dca76e27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c172aa65b292ed5b12",
+    "url": "/react-fontawesome/static/js/runtime~main.52c172aa.js"
+  },
+  {
+    "revision": "2f65d2ee3c9af69d911d",
+    "url": "/react-fontawesome/static/js/main.2f65d2ee.chunk.js"
+  },
+  {
+    "revision": "6ca02252a0a4a85cd416",
+    "url": "/react-fontawesome/static/js/1.6ca02252.chunk.js"
+  },
+  {
+    "revision": "2f65d2ee3c9af69d911d",
+    "url": "/react-fontawesome/static/css/main.62c870fd.chunk.css"
+  },
+  {
+    "revision": "67129ee33778564c38e069e941d34968",
+    "url": "/react-fontawesome/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac07df11173b574c58b793ddf89c5a3a.js"
+  "/react-fontawesome/precache-manifest.36651e83e60ef28d7a4428c6dca76e27.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-fontawesome/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
